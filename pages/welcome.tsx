b1f141b469690fba599b58140a273a76d3ddbf68
Junha Jang <query808@gmail.com>
2023-10-22 17:04:39 +0900

    no message

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
-import './welcome.css'; // CSS 스타일을 임포트합니다.
+import './welcome.css';  // CSS 파일을 임포트합니다.
 
 const Welcome = () => {
   const router = useRouter();
-
-  // 사용자 입력 데이터 상태
   const [formData, setFormData] = useState({
     name: '',
     department: '',
@@ -14,24 +12,24 @@ const Welcome = () => {
     class: ''
   });
 
-  // 폼 제출 시 실행되는 함수
+  // 폼 제출 핸들러
   const handleSubmit = (e) => {
     e.preventDefault();
-    Cookies.set('userData', JSON.stringify(formData)); // 사용자 데이터를 쿠키에 저장합니다.
-    router.push('/'); // 메인 페이지로 리디렉션 합니다.
+    Cookies.set('userData', JSON.stringify(formData));
+    router.push('/');  // index.tsx로 리디렉션
   };
 
-  // 폼 내용이 변경될 때 실행되는 함수
+  // 폼 변경 핸들러
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value }); // 변경된 폼 데이터를 상태에 저장합니다.
+    setFormData({ ...formData, [name]: value });
   };
 
   return (
     <div className="welcome-container">
       <div className="welcome-box">
-        <div className="logo-container">
-          <img src="/image/logo.svg" alt="Logo" className="logo" />
+      <div className="logo-container">
+            <img src="/image/logo.svg" alt="Logo" className="logo" />
         </div>
         <form onSubmit={handleSubmit} className="login-form">
           <div className="form-group">
@@ -46,34 +44,35 @@ const Welcome = () => {
             <br />
             <select name="department" value={formData.department} onChange={handleChange} required>
               <option value="" disabled>학과 선택</option>
-              {/* 학과 옵션들 */}
               <option value="컴퓨터디자인과">컴퓨터디자인과</option>
-              {/* ... */}
+              <option value="스마트소프트웨어과">스마트소프트웨어과</option>
+              <option value="인공지능프로그래밍과">인공지능프로그래밍과</option>
+              <option value="판매관리과">판매관리과</option>
+              <option value="사무행정과">사무행정과</option>
             </select>
           </div>
-
-          {/* 특정 학과에만 보이는 학년 선택 영역 */}
           {(formData.department !== '판매관리과' && formData.department !== '사무행정과') && (
             <div className="form-group">
               <select name="grade" value={formData.grade} onChange={handleChange} required>
                 <option value="" disabled>학년 선택</option>
-                {/* 학년 옵션들 */}
-                {/* ... */}
+                <option value="1">1학년</option>
+                <option value="2">2학년</option>
+                {formData.department !== '컴퓨터디자인과' && formData.department !== '인공지능프로그래밍과' && (
+                  <option value="3">3학년</option>
+                )}
               </select>
             </div>
           )}
-
-          {/* 특정 학과에만 보이는 반 선택 영역 */}
           {(formData.department !== '판매관리과' && formData.department !== '사무행정과') && (
             <div className="form-group">
               <select name="class" value={formData.class} onChange={handleChange} required>
                 <option value="" disabled>반 선택</option>
-                {/* 반 옵션들 */}
-                {/* ... */}
+                <option value="1">1반</option>
+                <option value="2">2반</option>
+                <option value="3">3반</option>
               </select>
             </div>
           )}
-
           <button type="submit" className="login-button">로그인</button>
         </form>
       </div>
